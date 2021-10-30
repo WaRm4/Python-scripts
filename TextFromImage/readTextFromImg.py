138d36b9c86a9434be3b44e455cc835a749b0efa
Killian <killian_meunier@outlook.fr>
2021-10-30 15:40:23 +0200

    ajout de threads dans le programme

--- TextFromImage/readTextFromImg.py
+++ TextFromImage/readTextFromImg.py
@@ -7,6 +7,7 @@ from pytesseract import *
 from os import listdir, mkdir
 from os.path import exists, abspath
 import re
+import threading
 
 # If you don't have tesseract executable in your PATH, include the following:
 # pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
@@ -18,23 +19,32 @@ srcDir = "./src/"
 outDir = "./out"
 # Regular expression to select files
 regex = "(.*bmp)|(.*png)|(.*jpg)"
-# every file in srcDir that respect the regular expression
+# Every file in srcDir that respect the regular expression
 files = [f for f in listdir(srcDir) if re.search(regex, f) != None]
 
-print(files)
 if not exists(outDir):
     mkdir(outDir)
 
+# Converting the file "fi" and writing the result
+def job(fi):
+    print("Processing " + fi + "...\n")
+    result = pytesseract.image_to_string(srcDir + fi)
+    outFile = open(outDir + "/" + fi.rsplit('.')[0] + ".txt", "w")
+    outFile.write(result)
+    outFile.close()
+
 print("Starting conversion...\n(of all image files located in " + abspath(srcDir) + "\n")
 
+threads = []
 if len(files) > 0:
     for f in files:
-        print("Processing " + f + "...")
-        result = pytesseract.image_to_string(srcDir + f)
-        outFile = open(outDir + "/" + f.rsplit('.')[0] + ".txt", "w")
-        outFile.write(result)
-        outFile.close()
+        t = threading.Thread(target=job, args=(f,))
+        threads.append(t)
+        t.start()
 
+    for thread in threads:
+        thread.join()
+        
     print("Done !")
 
 else:
